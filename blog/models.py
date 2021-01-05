5593386897ee0c9200c23ea55fb0906fda0777ad
smrha <lord.smrha@gmail.com>
2021-01-06 00:29:13 +0330

    Add bootstrap admin panel

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,14 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length=256)
 	content = models.TextField()
-	# add slug field
+	slug = models.SlugField(unique=True, allow_unicode=True)
 	# add status field
 	# add publish field
 	# add update field
 	# add auther field
+
+	class Meta:
+		unique_together = ('title', 'slug')
 
 	def __str__(self):
 		return self.title 
